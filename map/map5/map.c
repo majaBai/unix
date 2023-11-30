ebe0f2be96ac40b32f257c631c98dbf832dee8b0
majaBai <452712285@qq.com>
2023-11-30 17:20:48 +0800

    amap优化，hash函数

---
+++ map/map5/map.c
@@ -0,0 +1,128 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "map.h"
+/*
+现在创建 map 的时候会让 data 里的每个元素都 AArray_new 一次
+可以改为默认它是 NULL，使用中按需创建
+*/
+static unsigned int HASH_VAL = -1;
+
+unsigned int
+_hash(AString *key) {
+    // 简陋直接 ascii 码相加
+    // unsigned int r = 0;
+    // for(int i = 0; i < key->length; i++) {
+    //     char c = AString_charAt(key, i);
+    //     r += c;
+    //     r *= 10;
+    // }
+    // return r;
+
+    //直接返回递增的值
+    // 0.52s 降到 0.21s
+     HASH_VAL++;
+     return HASH_VAL;
+}
+AMap *
+Amap_newWithCap(int cap) {
+    AMap *m = malloc(sizeof(AMap));
+    m->capacity = cap;
+    m->count = 0;
+    m->data = malloc(sizeof(AArray *) * m->capacity);
+    for(int i =0; i < m->capacity; i++) {
+        // m->data[i] = AArray_new();
+        m->data[i] = NULL;
+    }
+    return m;
+}
+AMap *
+AMap_new(void){
+    return Amap_newWithCap(10);
+}
+void
+_rehash(AMap *m){
+    // printf("rehash--- \n");
+   AMap *m1 = Amap_newWithCap(m->capacity * 2);
+   for(int i = 0; i < m->capacity; i++) {
+    if(m->data[i] == NULL) {
+        continue;
+    }
+    AArray *data = m->data[i];
+    for(int j = 0; j < AArray_length(data); j++) {        
+        AArray * kv = AArray_get(data, j);        
+        AString *k = AArray_get(kv, 0);
+        AString *v = AArray_get(kv, 1);
+        AMap_set(m1, k, v);
+    }
+   }
+   
+   free(m->data);
+   m->data = m1->data;
+   m->capacity = m1->capacity;
+   free(m1);
+}
+void
+AMap_set(AMap *m, AString *key, AString *value) {
+    // printf("set1--- \n");
+    if((float)(m->count) / (float)m->capacity > 0.8) {
+        _rehash(m);
+    }
+     unsigned int idx = _hash(key) % (m->capacity);
+     if(m->data[idx] == NULL) {
+        m->data[idx] = AArray_new();
+     }
+    //  printf("set--- \n");
+     AArray *data = m->data[idx]; 
+     // 遍历是否key存在
+     for(int i = 0; i < AArray_length(data); i++) {
+        AArray *kv = AArray_get(data, i);
+        AString *k = AArray_get(kv, 0);
+        if(AString_equals(key, k)) {
+            AArray_set(kv, 1, value);
+            return;
+        }
+     }
+     AArray *kv = AArray_new();
+     AArray_add(kv, key);
+     AArray_add(kv, value);
+     AArray_add(data, kv);
+
+     m->count += 1;
+}
+
+AString *
+AMap_get(AMap *m, AString *key){
+    unsigned int idx = _hash(key) % (m->capacity);
+    AArray *data = m->data[idx];
+    for(int i = 0; i < AArray_length(data); i++) {
+        AArray *kv = AArray_get(data, i);
+        AString *k = AArray_get(kv, 0);
+        if(AString_equals(key, k)) {
+            return AArray_get(kv, 1);
+        }
+     }
+     return NULL;
+}
+
+bool
+AMap_has(AMap *m, AString *key){
+   unsigned int idx = _hash(key) % (m->capacity);
+    AArray *data = m->data[idx];
+    for(int i = 0; i < AArray_length(data); i++) {
+        AArray *kv = AArray_get(data, i);
+        AString *k = AArray_get(kv, 0);
+        if(AString_equals(key, k)) {
+            return true;
+        }
+     }
+     return false;
+}
+
+void
+AMap_destroy(AMap* m) {
+    free(m->data);
+    free(m);
+}
---
+++ map/map5/test.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+#include "map.h"
+/*
+在上一个基础上，我们可以预先创建 40w 个 AString，免去在循环中频繁 new 的开销
+这里不应该走 AString_new 接口，它一次只申请一小块内存
+我们应该手动一次性申请 40w 个 AString 那么大的内存
+*/
+// #include "../lib/alist.h"
+
+// cc map.c ../lib/*.c test.c -Ilib && ./a.out
+
+// 优化 cc -O3  map.c ../lib/*.c test.c -Ilib && ./a.out
+// valgrind 检查内存泄露
+// cat r2.txt|sort > 2.sort 将 r2.txt 排序并输入到 2.sort
+void
+main() {
+    int begin,end;
+    begin=clock();
+
+     // 一次性读取文件所有内容
+    long file_size;
+    char *allFile;
+    FILE *fp = fopen("../words2.txt", "r");
+    if (fp == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    fseek(fp, 0L, SEEK_END); // 将文件指针移到文件末尾
+    file_size = ftell(fp); // 获取文件大小
+    rewind(fp); // 将文件指针移到文件开头
+    allFile = (char*) malloc(file_size); 
+    // 动态分配内存
+    if (allFile == NULL) {
+        printf("内存分配失败\n");
+        exit(2);
+    }
+    fread(allFile, file_size, 1, fp); // 读取文件内容到内存中
+    fclose(fp); // 关闭文件
+
+    
+    AMap * mp = Amap_newWithCap(40 * 10000);
+    AString **allStr;
+    allStr = (AString **)malloc(40 * 10000 * sizeof(AString *));
+    for(int i = 0; i < 40 * 10000; i++) {
+        allStr[i] = AString_new("");
+    }
+
+    int j = 0; // 一个单词的长度
+    int count = 0;
+    for(int i = 0; i < file_size;){
+        if(allFile[i] == '\n') {
+            char *temp =  (char *)malloc(j + 1);
+            strncpy(temp, allFile + i - j, j);
+            temp[j] = '\0';
+            
+            // AString * s = AString_new(temp);
+            allStr[count]->data = temp;
+            AMap_set(mp, allStr[count], NULL);
+            j = 0;
+            count++;
+        }
+        j++;
+        i++;
+    }
+
+    end=clock();
+    printf("time= %f s\n",(float)(end-begin)/CLOCKS_PER_SEC);
+    printf("count= %d\n",count);
+
+    // 看看 AMap 容量的占用情况
+    {
+        for(int i = 0; i < mp->capacity; i++) {
+            if(mp->data[i] == NULL) {
+                continue;
+            }
+            AArray *d = mp->data[i];
+            unsigned int len = AArray_length(d);
+            if(len > 0) {
+                printf("%u \n",len);
+            }
+            
+        }
+    }
+
+    AMap_destroy(mp);
+}
